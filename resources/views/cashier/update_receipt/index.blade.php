cb2a62f273ee669b5d8bd89d12e4bdbc593d88f7
Sean Ronquillo <mervinronquillo@gmail.com>
2021-08-23 15:14:35 +0800

    updating of OR

--- resources/views/cashier/update_receipt/index.blade.php
+++ resources/views/cashier/update_receipt/index.blade.php
@@ -33,7 +33,7 @@ function get_value($id,$reference_id){
 </li>
 @endsection
 @section('header')
-<!--<h1>Module not yet done...</h1>-->
+<h1>Module not yet done...</h1>
 <section class="content-header">
       <h1>
         Update OR Number: {{$payment->receipt_no}}
